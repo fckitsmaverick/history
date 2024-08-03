9fa1a2bd8e762ce060f7184e17e08d899b65b042
Gabriel Besson <gabriel.bessonpro@gmail.com>
2024-08-03 14:49:38 +0900

    Basic quizz

---
+++ history.py
@@ -0,0 +1,8 @@
+print('Hello World')
+ans = input('What do you want ? ')
+ans = ans.lower()
+print(ans)
+if ans == "nothing":
+    print('Then fuck off')
+else:
+    print("Oh interesting, fuck off")
